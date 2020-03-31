521b36de8b60066ad53367ceb6dcae49133e36f0
Will Scott <will@cypherpunk.email>
2020-03-31 12:04:24 -0700

    use netroute hint rather than just network class /loopback-ness

--- multidialer.go
+++ multidialer.go
@@ -2,15 +2,15 @@ package tcpreuse
 
 import (
 	"context"
-	"fmt"
 	"math/rand"
 	"net"
+
+	"github.com/libp2p/go-netroute"
 )
 
 type multiDialer struct {
-	loopback    []*net.TCPAddr
-	unspecified []*net.TCPAddr
-	global      *net.TCPAddr
+	listeners map[*listener]struct{}
+	fallback  net.IP
 }
 
 func (d *multiDialer) Dial(network, addr string) (net.Conn, error) {
@@ -29,82 +29,47 @@ func (d *multiDialer) DialContext(ctx context.Context, network, addr string) (ne
 	if err != nil {
 		return nil, err
 	}
-
-	// We pick the source *port* based on the following algorithm.
-	//
-	// 1. If we're dialing loopback, choose a source-port in order of
-	//    preference:
-	//   1. A port in-use by an explicit loopback listener.
-	//   2. A port in-use by a listener on an unspecified address (must
-	//      also be listening on localhost).
-	//   3. A port in-use by a listener on a global address. We don't have
-	//      any other better options (other than picking a random port).
-	// 2. If we're dialing a global address, choose a source-port in order
-	//    of preference:
-	//   1. A port in-use by a listener on an unspecified address (the most
-	//      general case).
-	//   2. A port in-use by a listener on the global address.
-	// 3. Fail on link-local dials (go-ipfs currently forbids this and I
-	//    figured we could try lifting this restriction later).
-	//
-	//
-	// Note: We *always* dial from the unspecified address (regardless of
-	// the port we pick). In the future, we could use netlink (on Linux) to
-	// figure out the right source address but we're going to punt on that.
-
 	ip := tcpAddr.IP
-	source := d.global
-	switch {
-	case ip.IsLoopback():
-		switch {
-		case len(d.loopback) > 0:
-			source = randAddr(d.loopback)
-		case len(d.unspecified) > 0:
-			source = randAddr(d.unspecified)
-		}
-	case ip.IsGlobalUnicast():
-		switch {
-		case len(d.unspecified) > 0:
-			source = randAddr(d.unspecified)
-		}
-	default:
-		return nil, fmt.Errorf("undialable IP: %s", tcpAddr.IP)
+
+	router, err := netroute.New()
+	if err != nil {
+		return nil, err
 	}
-	return reuseDial(ctx, source, network, addr)
+
+	_, _, preferredSrcIP, err := router.Route(ip)
+	if err != nil {
+		return nil, err
+	}
+
+	unspecifiedCandidates := make([]*net.TCPAddr, 0)
+	existingPort := 0
+	for l := range d.listeners {
+		optAddr := l.Addr().(*net.TCPAddr)
+		if optAddr.IP.Equal(preferredSrcIP) {
+			return reuseDial(ctx, optAddr, network, addr)
+		}
+		if optAddr.IP.IsUnspecified() {
+			if optAddr.Network() == network {
+				unspecifiedCandidates = append([]*net.TCPAddr{optAddr}, unspecifiedCandidates...)
+			} else {
+				unspecifiedCandidates = append(unspecifiedCandidates, optAddr)
+			}
+		}
+		if optAddr.Network() == network {
+			existingPort = optAddr.Port
+		}
+	}
+	if len(unspecifiedCandidates) == 0 {
+		unspecifiedCandidates = []*net.TCPAddr{&net.TCPAddr{IP: d.fallback, Port: existingPort, Zone: ""}}
+	}
+
+	return reuseDial(ctx, unspecifiedCandidates[0], network, addr)
 }
 
-func newMultiDialer(unspec net.IP, listeners map[*listener]struct{}) dialer {
-	m := new(multiDialer)
-	for l := range listeners {
-		laddr := l.Addr().(*net.TCPAddr)
-		switch {
-		case laddr.IP.IsLoopback():
-			m.loopback = append(m.loopback, laddr)
-		case laddr.IP.IsGlobalUnicast():
-			// Different global ports? Crap.
-			//
-			// The *proper* way to deal with this is to, e.g., use
-			// netlink to figure out which source address we would
-			// normally use to dial a destination address and then
-			// pick one of the ports we're listening on on that
-			// source address. However, this is a pain in the ass.
-			//
-			// Instead, we're just going to always dial from the
-			// unspecified address with the first global port we
-			// find.
-			//
-			// TODO: Port priority? Addr priority?
-			if m.global == nil {
-				m.global = &net.TCPAddr{
-					IP:   unspec,
-					Port: laddr.Port,
-				}
-			} else {
-				log.Warning("listening on external interfaces on multiple ports, will dial from %d, not %s", m.global, laddr)
-			}
-		case laddr.IP.IsUnspecified():
-			m.unspecified = append(m.unspecified, laddr)
-		}
+func newMultiDialer(unspec net.IP, listeners map[*listener]struct{}) (m dialer) {
+	m = &multiDialer{
+		listeners: listeners,
+		fallback:  unspec,
 	}
-	return m
+	return
 }
